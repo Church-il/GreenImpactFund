a0802d8cc41211b70f5d24b737da94ed794d5a2b
Michael Akoko <akokomichael37@gmail.com>
2025-02-16 15:55:02 +0300

    render new shellfile

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Update package list and install libgirepository1.0-dev
-apt-get update && apt-get install -y libgirepository1.0-dev
-
 # Install Python dependencies using pipenv
 pipenv install -r requirements.txt
