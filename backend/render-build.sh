74b0103be03500d3f04a56f45dc0f12d193e137a
Michael Akoko <akokomichael37@gmail.com>
2025-02-16 17:50:18 +0300

    render new-2 shellfile

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Install system dependencies
-apt-get update -y
-apt-get install -y libgirepository1.0-dev
-
-# Install Python dependencies using pipenv
-pipenv install -r requirements.txt
+# Install Python dependencies using poetry
+poetry install
